1e394527c285cb848a7e38968b3422601d6a0bfe
viduraqcomit <vidura@qcomit.com>
2025-02-09 11:32:51 +0530

    Modified db connection class.

--- src/main/java/com/example/cabservice/config/DatabaseConnectionManager.java
+++ src/main/java/com/example/cabservice/config/DatabaseConnectionManager.java
@@ -6,11 +6,19 @@ public class DatabaseConnectionManager {
     private static DatabaseConnectionManager instance;
     private Connection connection;
 
-    private DatabaseConnectionManager() throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cab_service?createDatabaseIfNotExist=true", "root", "1234");
+    private DatabaseConnectionManager()  {
+        System.out.println("Logger: DatabaseConnectionManager.Constructor - Start");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cab_service?createDatabaseIfNotExist=true", "root", "1234");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Logger: DatabaseConnectionManager.Constructor - Error occured while connecting to database:" + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+        System.out.println("Logger: DatabaseConnectionManager.Constructor - End");
     }
 
-    public static synchronized DatabaseConnectionManager getInstance() throws SQLException {
+    public static synchronized DatabaseConnectionManager getInstance() {
         if (instance == null) {
             instance = new DatabaseConnectionManager();
         }
